a9ae37f535ca36a914a4356d809de652ae0406fa
Ameer Hamza <xovolt.hamza@gmail.com>
2019-03-17 14:59:50 +0500

    syntax error

--- database/migrations/2019_03_16_071749_create_clinics_table.php
+++ database/migrations/2019_03_16_071749_create_clinics_table.php
@@ -17,7 +17,7 @@ class CreateClinicsTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email');
-            $table->string('phone_numbers')
+            $table->string('phone_numbers');
             $table->timestamps();
         });
     }
